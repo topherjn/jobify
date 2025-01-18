93f2d750bfeb3617b6c30d89f2e8bc390d30ec14
topherjn <nicholsonc@gmail.com>
2025-01-18 18:49:03 -0500

    Edit job using app.patch Step 67

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ let jobs = [
   { id: nanoid(), company: 'google', position: 'back-end  ' },
 ];  
 
-
-
 const app = express();
 
 dotenv.config();
@@ -19,10 +17,6 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 const port = process.env.PORT || 5100; 
-
-app.listen(port, () => {
-  console.log(`Server running on PORT ${port}`);
-});
 
 app.use(express.json());
 
@@ -36,8 +30,51 @@ app.post('/', (req, res) => {
   res.json({ message: 'Data received', data: req.body });
 }); 
 
+// Get all jobs
 app.get('/api/v1/jobs', (req, res) => {
   res.status(200).json({ jobs });
 });
 
+// Create a job
 
+app.post('/api/v1/jobs', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ error: 'company and position are required' });
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  
+  res.status(200).json({ job });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on PORT ${port}`);
+});
+
+// Get a single job 
+// app.get('/api/v1/jobs/:id', (req, res) => {
+//   const { id } = req.params;
+//   const job = jobs.find((job) => job.id === id);
+//   if (!job) {
+//     return res.status(404).json({ error: 'job not found' });
+//   }
+//   res.status(200).json({ job });
+// });
+
+// edit job
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { company, position } = req.body;
+  if(!company || !position) {
+    return res.status(400).json({ error: 'company or position is required' });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ error: 'job not found' });
+  }
+  job.company = company;
+  job.position = position;
+  res.status(200).json({msg: 'job modified', job});
+});
