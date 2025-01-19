8acc8e75b0293403884cd55711c59a4b8018274c
topherjn <nicholsonc@gmail.com>
2025-01-19 17:29:20 -0500

    Get all and single job Step 79

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -7,44 +7,26 @@ let jobs = [
   { id: nanoid(), company: 'google', position: 'back-end  ' },
 ];  
 
+
+// parameter empty on find gets all jobs
+// can get a single job with find({id: id  })   
 export const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({});
     res.status(200).json({ jobs });
 };
 
-// export const createJob = async (req, res) => {  
-//     try {
-//         const { company, position } = req.body;
-//         const job = await Job.create({ company, position });
-//         res.status(200).json({ job });
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({ error: 'Server error' });
-//     }
 
-// }
-
+// async errors lib takes care of try catch block
 export const createJob = async (req, res) => {  
     const { company, position } = req.body;
     const job = await Job.create({ company, position });
     res.status(200).json({ job });
-
 }
 
-// export const createJob = async (req, res) => {        
-//     const { company, position } = req.body;
-//     if (!company || !position) {
-//         return res.status(400).json({ error: 'company and position are required' });
-//     }
-//     const id = nanoid(10);
-//     const job = { id, company, position };
-//     jobs.push(job);
-    
-//     res.status(200).json({ job });
-// };
 
 export const getJob = async (req, res) => {
     const { id } = req.params;
-    const job = jobs.find((job) => job.id === id);
+    const job = await Job.findById(id);
     if (!job) {
         return res.status(404).json({ error: 'job not found' });
     }
