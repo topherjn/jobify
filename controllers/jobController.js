e9bd24f4baeb49956d7905fd215960c95584511c
topherjn <nicholsonc@gmail.com>
2025-01-19 17:42:00 -0500

    Status codes lib added Step 81

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,13 +1,14 @@
 import 'express-async-errors';
 import Job from '../models/JobModel.js';
 import { nanoid } from 'nanoid';
+import { StatusCodes } from 'http-status-codes';
 
 
 // parameter empty on find gets all jobs
 // can get a single job with find({id: id  })   
 export const getAllJobs = async (req, res) => {
     const jobs = await Job.find({});
-    res.status(200).json({ jobs });
+    res.status(StatusCodes.OK).json({ jobs });
 };
 
 
@@ -15,7 +16,7 @@ export const getAllJobs = async (req, res) => {
 export const createJob = async (req, res) => {  
     const { company, position } = req.body;
     const job = await Job.create({ company, position });
-    res.status(200).json({ job });
+    res.status(StatusCodes.CREATED).json({ job });
 }
 
 
@@ -23,19 +24,19 @@ export const getJob = async (req, res) => {
     const { id } = req.params;
     const job = await Job.findById(id);
     if (!job) {
-        return res.status(404).json({ error: 'job not found' });
+        return res.status(StatusCodes.NOT_FOUND).json({ error: 'job not found' });
     }
-    res.status(200).json({ job });
+    res.status(StatusCodes.OK).json({ job });
 };
 
 export const updateJob = async (req, res) => {    
     const { id } = req.params;
     const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true });
     if(!updateJob) {
-        return res.status(404).json({ error: 'job not found' });
+        return res.status(StatusCodes.NOT_FOUND).json({ error: 'job not found' });
     }
 
-    res.status(200).json({msg: 'job modified', updatedJob});
+    res.status(StatusCodes.OK).json({msg: 'job modified', updatedJob});
 };
 
 export const deleteJob = async (req, res) => {
@@ -43,7 +44,7 @@ export const deleteJob = async (req, res) => {
     const removedJob = await Job.findByIdAndDelete(id);
 
     if(!removedJob) {
-        return res.status(404).json({ error: 'job not found' });
+        return res.status(StatusCodes.NOT_FOUND).json({ error: 'job not found' });
     }   
     res.status(200).json({msg: 'job removed', removedJob});
 };
