6b7974879f640a4bb0d30a25b9caa1b64e3e821d
topherjn <nicholsonc@gmail.com>
2025-01-20 13:56:06 -0500

    Register / Create User started Step 93

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,11 @@
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/UserModel.js';
+
 export const register = async (req, res) => {
-    res.send('register');
-  };
-  export const login = async (req, res) => {
-    res.send('login');
-  };
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
+};
+
+export const login = async (req, res) => {
+  res.send('login');
+};
