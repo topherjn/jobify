f15d6bb44cd83e949a0d806d785c19c26dda5a55
topherjn <nicholsonc@gmail.com>
2025-01-20 14:28:55 -0500

    Hashing password moved to utility Step 97

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,13 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
-import bcrypt from 'bcryptjs';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
-  const isFirstAccount = (await User.countDocuments()) === 0;
-  req.body.role = isFirstAccount ? 'admin' : 'user';
-  // hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ msg: "User created" });
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
 
 export const login = async (req, res) => {
