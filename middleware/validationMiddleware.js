fcd2565d6cc410a3daeeee9d6824ed457da12636
topherjn <nicholsonc@gmail.com>
2025-01-20 14:08:21 -0500

    User validation added in Register Step 94

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -3,6 +3,7 @@ import { BadRequestError, NotFoundError } from '../errors/customErrors.js';
 import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
 import mongoose from 'mongoose';
 import Job from '../models/JobModel.js';
+import User from '../models/UserModel.js';
 
 // once we have this function we can tailor it to specific situations
 // use model schemata as a guide
@@ -33,12 +34,34 @@ export const validateIdParam = withValidationErrors([
 ]);
 
 export const validateJobInput = withValidationErrors([
-    body('company').notEmpty().withMessage('company is required'),
-    body('position').notEmpty().withMessage('position is required'),
-    body('jobLocation').notEmpty().withMessage('job location is required'),
-    body('jobStatus')
-      .isIn(Object.values(JOB_STATUS))
-      .withMessage('invalid status value'),
-    body('jobType').isIn(Object.values(JOB_TYPE)).withMessage('invalid job type'),
-  ]);
+  body('company').notEmpty().withMessage('company is required'),
+  body('position').notEmpty().withMessage('position is required'),
+  body('jobLocation').notEmpty().withMessage('job location is required'),
+  body('jobStatus')
+    .isIn(Object.values(JOB_STATUS))
+    .withMessage('invalid status value'),
+  body('jobType').isIn(Object.values(JOB_TYPE)).withMessage('invalid job type'),
+]);
+
+export const validateRegisterInput = withValidationErrors([
+  body('name').notEmpty().withMessage('name is required'),
+  body('email')
+    .notEmpty()
+    .withMessage('email is required')
+    .isEmail()
+    .withMessage('invalid email format')
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new BadRequestError('email already exists');
+      }
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 8 })
+    .withMessage('password must be at least 8 characters long'),
+  body('location').notEmpty().withMessage('location is required'),
+  body('lastName').notEmpty().withMessage('last name is required'),
+]);
 
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { register, login } from '../controllers/authController.js';
+import { validateRegisterInput } from '../middleware/validationMiddleware.js';
+
 const router = Router();
 
-router.post('/register', register);
+router.post('/register', validateRegisterInput, register);
 router.post('/login', login);
 
 export default router;
