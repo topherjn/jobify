9a5202fd4df96de83bba84fcea63cc56817660df
topherjn <nicholsonc@gmail.com>
2025-01-20 16:39:46 -0500

    Verify JWT implemented Step 105

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -4,9 +4,9 @@ import { StatusCodes } from 'http-status-codes';
 
 // C - CREATE JOB
 // async errors lib takes care of try catch block
-export const createJob = async (req, res) => {  
-    const { company, position } = req.body;
-    const job = await Job.create({ company, position });
+export const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create(req.body);
     res.status(StatusCodes.CREATED).json({ job });
 };
 
@@ -14,7 +14,8 @@ export const createJob = async (req, res) => {
 // parameter empty on find gets all jobs
 // can get a single job with find({id: id  })   
 export const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({});
+    console.log(req.user);
+    const jobs = await Job.find({ createdBy: req.user.userId });
     res.status(StatusCodes.OK).json({ jobs });
 };
 
@@ -25,13 +26,13 @@ export const getJob = async (req, res) => {
 };
 
 // U - UPDATE JOB
-export const updateJob = async (req, res) => {    
+export const updateJob = async (req, res) => {
     const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.status(StatusCodes.OK).json({msg: 'job modified', updatedJob});
+    res.status(StatusCodes.OK).json({ msg: 'job modified', updatedJob });
 };
 
 // D - DELETE JOB
 export const deleteJob = async (req, res) => {
     const removedJob = await Job.findByIdAndDelete(req.params.id);
-    res.status(200).json({msg: 'job removed', removedJob});
+    res.status(200).json({ msg: 'job removed', removedJob });
 };
