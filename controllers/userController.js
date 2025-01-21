d6a4fbe0eb1da9782803f4556155284109c87c85
topherjn <nicholsonc@gmail.com>
2025-01-20 19:05:18 -0500

    User update complete Step 111

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,5 +12,9 @@ export const getApplicationStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'update user' });
+  const obj = {... req.body};
+  delete obj.password;
+  console.log(obj);
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, req.body);
+  res.status(StatusCodes.OK).json({ msg: 'user updated' });
 };
