6046d4b511ff23f683e0ef5633e8a554e38a0a92
topherjn <nicholsonc@gmail.com>
2025-01-22 14:46:05 -0500

    Add job form structure implemented Step 126

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { FormRow } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useOutletContext } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
 
 const AddJob = () => {
-  return (
-    <h1>
-      Add Job Page
-    </h1>
-  )
-}
+  const { user } = useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
 
-export default AddJob
+  return (
+    <Wrapper>
+      <Form method='post' className='form'>
+        <h4 className='form-title'>add job</h4>
+        <div className='form-center'>
+          <FormRow type='text' labelText='Position' name='position' />
+          <FormRow type='text' labelText = 'Company' name='company' />
+          <FormRow
+            type='text'
+            labelText='job location'
+            name='jobLocation'
+            defaultValue={user.location}
+          />
+
+          <button
+            type='submit'
+            className='btn btn-block form-btn '
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'submitting...' : 'submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default AddJob;
