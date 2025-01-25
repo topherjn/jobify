2cfa1ef0042e4c9d89ff31f9d5b4fef69ef0a898
topherjn <nicholsonc@gmail.com>
2025-01-25 18:36:42 -0500

    Onchanges added to all form elements in Step 173

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -5,6 +5,7 @@ import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from '../../../utils/constants';
 import { useAllJobsContext } from '../pages/AllJobs';
 
 const SearchContainer = () => {
+  const submit = useSubmit();
   return (
     <Wrapper>
       <Form className='form'>
@@ -17,23 +18,27 @@ const SearchContainer = () => {
             name="search"
             labelText="Search"
             defaultValue='a'
+            onChange={(e) => {submit(e.currentTarget.form)}}
           />
           <FormRowSelect
             labelText='job status'
             name='jobStatus'
             list={['all', ...Object.values(JOB_STATUS)]}
             defaultValue='all'
+            onChange={(e) => {submit(e.currentTarget.form)}}
           />
           <FormRowSelect
             labelText='job type'
             name='jobType'
             list={['all', ...Object.values(JOB_TYPE)]}
             defaultValue='all'
+            onChange={(e) => {submit(e.currentTarget.form)}}
           />
           <FormRowSelect
             name='sort'
             list={[...Object.values(JOB_SORT_BY)]}
             defaultValue='newest'
+            onChange={(e) => {submit(e.currentTarget.form)}}
           />
 
           <Link to='/dashboard/all-jobs' className='btn form-btn delete-btn'>
