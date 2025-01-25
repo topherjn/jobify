6672845190ea9554dbf17e18c9023d6f874fb42a
topherjn <nicholsonc@gmail.com>
2025-01-24 21:38:43 -0500

    Stats route set up Step 158

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,8 @@
 import 'express-async-errors';
 import Job from '../models/JobModel.js';
 import { StatusCodes } from 'http-status-codes';
+import mongoose from 'mongoose';
+import day from 'dayjs';
 
 // C - CREATE JOB
 // async errors lib takes care of try catch block
@@ -21,7 +23,7 @@ export const getAllJobs = async (req, res) => {
 
 // R - GET JOB
 export const getJob = async (req, res) => {
-    req.body.createdBy= req.user.userId;
+    req.body.createdBy = req.user.userId;
     console.log(req.params.id);
     const job = await Job.findById(req.params.id);
     console.log(job);
@@ -39,5 +41,27 @@ export const deleteJob = async (req, res) => {
     console.log(req.params.id);
     const removedJob = await Job.findByIdAndDelete(req.params.id);
     res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob });
-  };
-  
+};
+
+export const showStats = async (req, res) => {
+    const defaultStats = {
+        pending: 22,
+        interview: 11,
+        declined: 4
+    }
+    let monthlyApplications = [
+        {
+            date: 'May 23',
+            count: 12,
+        },
+        {
+            date: 'Jun 23',
+            count: 9,
+        },
+        {
+            date: 'Jul 23',
+            count: 3,
+        }
+    ];
+    res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
+};
