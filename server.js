48276875c9166bda1d46cf69b114069eec953a25
topherjn <nicholsonc@gmail.com>
2025-01-25 19:53:05 -0500

    Local build default values removed and server public distributable tested Step 180

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 
+app.get('*', (req, res) => {  
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
