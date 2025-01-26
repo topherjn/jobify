26bc7d74144aa610f0b5d19afbd0d2c1a09bb77b
topherjn <nicholsonc@gmail.com>
2025-01-26 13:53:09 -0500

    Installed React Query and dev tools but is it working? Step 186

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import { action as deleteJobAction } from './pages/DeleteJob';
 import { loader as adminLoader } from './pages/Admin';
 import {action as profileAction} from './pages/Profile';
 import {loader as statsLoader} from './pages/Stats';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import {
   HomeLayout,
   Landing,
@@ -33,6 +35,14 @@ export const checkDefaultTheme = () => {
 }
 
 const isDarkThemeEnabled = checkDefaultTheme();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -81,6 +91,12 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
+
 export default App;
